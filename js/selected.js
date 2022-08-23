fa300b98620093608db3dfb7b207ca670c1a30e8
Ashraful Alam <ashrafcse3@gmail.com>
2022-08-23 10:55:33 +0100

    added no more than 5 people feature

--- js/selected.js
+++ js/selected.js
@@ -16,18 +16,24 @@ function displaySelectedPlayer(names) {
 function playerSelect(element) {
     // fetch the player name in onclick
     const playerName = element.parentNode.parentNode.children[0].innerText;
-    selectedPlayer.push(playerName);
 
-    // disable the clicked button
-    const clickedButton = element;
-    element.setAttribute('disabled', '');
-    // change the button background color and text color
-    element.style.backgroundColor = 'grey';
-    element.style.color = 'white';
+    // give a warning after adding 5 people
+    if (selectedPlayer.length === 5) {
+        alert('You cannot add more than 5 people');
+        return;
+    }
+    else {
+        selectedPlayer.push(playerName);
+        // disable the clicked button
+        const clickedButton = element;
+        clickedButton.setAttribute('disabled', '');
+        // change the button background color and text color
+        clickedButton.style.backgroundColor = 'grey';
+        clickedButton.style.color = 'white';
 
-    // add the player name into the selected section
-    // console.log();
-    displaySelectedPlayer(selectedPlayer);
+        // add the player name into the selected section
+        displaySelectedPlayer(selectedPlayer);
+    }
 }
 
 const selectedPlayer = [];
